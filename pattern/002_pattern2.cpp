0fc851ec58cf1873873fe4d3c7ea43750d874d38
ishefalichopra <isshefalichopra900@gmail.com>
2023-04-12 20:41:49 +0530

    pattern 2

---
+++ pattern/002_pattern2.cpp
@@ -0,0 +1,24 @@
+// 111
+// 222
+// 333
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i;
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+
+return 0;
+}
