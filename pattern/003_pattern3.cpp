4a905fdae43cd085ae22d1cea08a868a4c1c9706
ishefalichopra <isshefalichopra900@gmail.com>
2023-04-12 20:44:48 +0530

    pattern 3

---
+++ pattern/003_pattern3.cpp
@@ -0,0 +1,22 @@
+//1234
+//1234
+//1234
+//1234
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+cin>>n;
+int i=1;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        cout<<j;
+        j+=1;
+    }
+    cout<<endl;
+    i+=1;
+}
+
+return 0;
+}
