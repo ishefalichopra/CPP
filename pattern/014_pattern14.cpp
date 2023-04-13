820bfec6ac58e9c45c7f6de55737be611d19283b
ishefalichopra <isshefalichopra900@gmail.com>
2023-04-13 20:46:19 +0530

    pattern 14

---
+++ pattern/014_pattern14.cpp
@@ -0,0 +1,22 @@
+//ABC
+//BCD
+//CDE
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int i=1;
+while(i<=n){
+    int j= 1;
+    while(j<=n){
+        char ch= 'A'+i+j-2;
+        cout<<ch;
+        j+=1;
+    }
+    cout<<endl;
+    i+=1;
+}
+return 0;
+}
