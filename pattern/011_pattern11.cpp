84a04ec6f81eb35fdc43cd0758885da0be553fe2
ishefalichopra <isshefalichopra900@gmail.com>
2023-04-13 20:42:15 +0530

    pattern 11

---
+++ pattern/011_pattern11.cpp
@@ -0,0 +1,25 @@
+// AAA
+// BBB
+// CCC
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            char ch = 'A' + (i - 1);
+            cout << ch;
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+    return 0;
+}
