fdee25a508bdb2f8b302ee17b52a9c9d738637a8
ishefalichopra <shefalichopra900@gmail.com>
2023-05-20 18:56:22 +0530

    complement of a base 10 integer

---
+++ 095_complementOf10Int.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int m = n;
+int mask = 0;
+while(m!=0){
+    mask = (mask<<1)|1;
+    m=m>>1;
+}
+int ans = (~n)&mask;
+cout<<ans;
+return 0;
+}
