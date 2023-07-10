960d42494168f45c0883902705003098e04fb67c
ishefalichopra <shefalichopra900@gmail.com>
2023-07-10 09:04:11 +0530

    pattern05 Striver Series

--- 117_pattern05.cpp
+++ 117_pattern05.cpp
@@ -13,7 +13,7 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         char c = 'A';
-        for (int j = 0; j <= (n - i); j++)
+        for (int j = 0; j < (n - i); j++)
         {
             cout << " ";
         }
