a35c94dd4454bd8e73d68c624a7825341ed35e90
ishefalichopra <shefalichopra900@gmail.com>
2023-07-15 13:08:35 +0530

    longest subarray with sum K

---
+++ 136_longestSubarraySumK.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<bits/stdc++.h>
+
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int n = a.size(); // size of the array.
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+    cin >> arr[i];}
+    long long k;
+    cin >> k;
+    cout<<longestSubarrayWithSumK(arr,k);
+
+return 0;
+}
