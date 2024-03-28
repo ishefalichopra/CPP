a6d0bf463acc8bde91dccdbb2f7a16cc0ca40088
ishefalichopra <isshefalichopra900@gmail.com>
2024-03-28 18:58:27 +0530

    check if subset

---
+++ 194_checkSubset.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    unordered_map<int, int> mp;
+    for(int i=0; i<n; i++){
+        mp[a1[i]]++;
+    }
+    
+    for(int i=0; i<m; i++){
+        if(mp[a2[i]]==0){
+            return "No";
+        }
+        else{
+            mp[a2[i]]--;
+        }
+    }
+    
+    return "Yes";
+}
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    int a1[n],a2[m];
+    for(int i=0;i<n;++i){
+        cin >> a1[i];
+    }
+    for(int i=0;i<m;++i){
+        cin >> a2[i];
+    }
+    cout << isSubset(a1,a2,n,m) << endl;
+
+
+return 0;
+}
