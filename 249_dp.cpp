bfd9eca7b5f0c86dd4991751db77e861e14e3978
ishefalichopra <isshefalichopra900@gmail.com>
2024-07-02 09:29:09 +0530

    fibonacci series using space optimization

--- 249_dp.cpp
+++ 249_dp.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // int fib(int n, vector<int>&dp){
-//     if(n<=1) 
+//     if(n<=1)
 //     return n;
 
 // //step 3
@@ -12,22 +12,25 @@ using namespace std;
 
 // //step2
 //     dp[n]= fib(n-1,dp)+fib(n-2,dp);
-//     return dp[n];    
+//     return dp[n];
 // }
-
 
 int main()
 {
     int n;
-    cin>>n;
-    vector<int>dp(n+1);
-    dp[0]=0;
-    dp[1]=1;
-    //step2
-    for(int i=2;i<=n;i++){
-        dp[i]= dp[i-1]+dp[i-2];
-    }
-    cout<<dp[n]<<endl;
+    cin >> n;
 
-return 0;
+    int prev2 = 0;
+    int prev1 = 1;
+    // step2
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    cout << prev1 << endl;
+
+    return 0;
 }
