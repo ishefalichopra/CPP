962a4f8090ef6a9a23e299a7fe769fab2788710e
ishefalichopra <isshefalichopra900@gmail.com>
2024-07-02 09:24:28 +0530

    fibonacci series by using top down dynamic programming

---
+++ 249_dp.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n, vector<int>&dp){
+    if(n<=1) 
+    return n;
+
+//step 3
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+//step2
+    dp[n]= fib(n-1,dp)+fib(n-2,dp);
+    return dp[n];    
+}
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>dp(n+1,-1);
+    cout<<fib(n,dp)<<endl;
+
+return 0;
+}
