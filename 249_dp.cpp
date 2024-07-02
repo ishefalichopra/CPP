29601beba054347b236c54e1db07cbb40920f1dc
ishefalichopra <isshefalichopra900@gmail.com>
2024-07-02 09:27:05 +0530

    fibonacci series using bottom up dp approach

--- 249_dp.cpp
+++ 249_dp.cpp
@@ -1,27 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fib(int n, vector<int>&dp){
-    if(n<=1) 
-    return n;
+// int fib(int n, vector<int>&dp){
+//     if(n<=1) 
+//     return n;
 
-//step 3
-    if(dp[n]!=-1){
-        return dp[n];
-    }
+// //step 3
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }
 
-//step2
-    dp[n]= fib(n-1,dp)+fib(n-2,dp);
-    return dp[n];    
-}
+// //step2
+//     dp[n]= fib(n-1,dp)+fib(n-2,dp);
+//     return dp[n];    
+// }
 
 
 int main()
 {
     int n;
     cin>>n;
-    vector<int>dp(n+1,-1);
-    cout<<fib(n,dp)<<endl;
+    vector<int>dp(n+1);
+    dp[0]=0;
+    dp[1]=1;
+    //step2
+    for(int i=2;i<=n;i++){
+        dp[i]= dp[i-1]+dp[i-2];
+    }
+    cout<<dp[n]<<endl;
 
 return 0;
 }
