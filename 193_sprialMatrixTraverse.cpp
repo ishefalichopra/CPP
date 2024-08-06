6c9d62944ff3438d921c60a159f5da87f4bde4aa
ishefalichopra <isshefalichopra900@gmail.com>
2024-08-06 12:34:06 +0530

    spiral matrix

--- 193_sprialMatrixTraverse.cpp
+++ 193_sprialMatrixTraverse.cpp
@@ -1,31 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> spiralTraverse(vector<vector<int>>matrix, int r, int c){
-    int top=0, bottom=r-1, right=c-1, left=0;
-    vector<int>ans;
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
 
-    while(left<=right&&top<=bottom){
+    if (matrix.empty() || matrix[0].empty())
+    {
+        return {};
+    }
 
-        for(int i=top;i<=right;i++){
+    int r = matrix.size();
+    int c = matrix[0].size();
+
+    int top = 0, bottom = r - 1, left = 0, right = c - 1;
+    vector<int> ans;
+
+    while (left <= right && top <= bottom)
+    {
+        for (int i = left; i <= right; i++)
+        {
             ans.push_back(matrix[top][i]);
         }
         top++;
 
-        for(int i=right;i<=bottom;i++){
+        for (int i = top; i <= bottom; i++)
+        {
             ans.push_back(matrix[i][right]);
         }
         right--;
 
-        if(top<=bottom){
-            for(int i=right;i>=left;i--){
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+            {
                 ans.push_back(matrix[bottom][i]);
             }
             bottom--;
         }
 
-        if(left<=right){
-            for(int i=bottom;i>=top;i--){
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+            {
                 ans.push_back(matrix[i][left]);
             }
             left++;
@@ -36,19 +52,17 @@ vector<int> spiralTraverse(vector<vector<int>>matrix, int r, int c){
 
 int main()
 {
-    int r,c;
-    cin >> r >> c;
-    vector<vector<int>>matrix(r,vector<int>(c));
-    for(int i=0;i<r;++i){
-        for(int j=0;j<c;++j){
-            cin >> matrix[i][j];
-        }
-    }
-    vector<int>ans=spiralTraverse(matrix,r,c);
-    for(auto x:ans){
-        cout << x << " ";
+    vector<vector<int>> matrix = {{1, 2, 3},
+                                  {4, 5, 6},
+                                  {7, 8, 9}};
+
+    vector<int> ans = spiralOrder(matrix);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
     }
     cout << endl;
 
-return 0;
+    return 0;
 }
