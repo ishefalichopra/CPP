7480ec19714f0bf852b9c6fd139ecf312126d066
ishefalichopra <isshefalichopra900@gmail.com>
2025-01-02 13:20:41 +0530

    Pattern 7 TuF

--- 369_Pattern8.cpp
+++ 369_Pattern8.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 int main()
 {
-
-int n = 5;
-
+    int n = 5;
     for (int i = 1; i <= n; i++)
     {
-
-        for (int j = 1; j <= n - i; j++)
+        for (int j = 1; j <= (i - 1); j++)
         {
             cout << " ";
         }
-        for (int j = 1; j <= 2 * i - 1; j++)
+        for (int j = 1; j <= 2 * (n - i + 1) - 1; j++)
         {
             cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
-    return 0;
+
+return 0;
 }
