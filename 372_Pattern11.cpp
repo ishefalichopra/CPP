0539e00bf58521fe38e1c41a63cd712899a42cf3
ishefalichopra <isshefalichopra900@gmail.com>
2025-01-03 12:07:36 +0530

    Pattern 11

---
+++ 372_Pattern11.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n =5;
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        for(int j=1;j<=2*(n-i-1);j++){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+return 0;
+}
