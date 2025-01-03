431cb32c7a9d75306eaa12cee05298142184b128
ishefalichopra <isshefalichopra900@gmail.com>
2025-01-03 13:16:24 +0530

    Pattern 17 TuF

---
+++ 376_Pattern17.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+int n = 5;
+int j,i;
+for(i=1;i<=n;i++){
+    char ch = 'A'+n-i;
+    for(j=1;j<=i;j++){
+        cout<<ch;
+        ch++;
+    }
+    cout<<endl;
+}
+return 0;
+}
