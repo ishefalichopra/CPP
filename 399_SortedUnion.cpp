1ea852c2f66b627834e7faa033894a2f991dd68a
ishefalichopra <shefalichopra900@gmail.com>
2025-01-30 19:03:11 +0530

    Union of two sorted arrays with duplicates

---
+++ 399_SortedUnion.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        map<int,int>hashh;
+        vector<int>ans;
+        int n1 = a.size(), n2 = b.size();
+        for(int i=0;i<n1;i++){
+            hashh[a[i]]++;
+        }
+        for(int i=0;i<n2;i++){
+            hashh[b[i]]++;
+        }
+        for(auto& x:hashh){
+            ans.push_back(x.first);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
