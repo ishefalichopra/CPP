8f6daff68fd8f636896aa31bdf6cfd6cc5fd5c24
ishefalichopra <isshefalichopra900@gmail.com>
2025-04-08 20:23:26 +0530

    optimized bit manipulation using BITWISE operators

--- 497_bitManipulation.cpp
+++ 497_bitManipulation.cpp
@@ -1,42 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void decimalToBinary(int n, int number[]){
-    for(int i = 31;i>=0;i--){
-         number[i] = n%2;
-         n /= 2;
-    }
-}
+// void decimalToBinary(int n, int number[]){
+//     for(int i = 31;i>=0;i--){
+//          number[i] = n%2;
+//          n /= 2;
+//     }
+// }
 
-int binaryToDecimal(int number[]){
-    int decimal = 0;
-    for(int i=0;i<32;i++){
-        decimal = decimal * 2 + number[i];
-    }
-    return decimal;
-}
+// int binaryToDecimal(int number[]){
+//     int decimal = 0;
+//     for(int i=0;i<32;i++){
+//         decimal = decimal * 2 + number[i];
+//     }
+//     return decimal;
+// }
 
-void bitManipulation(int num, int i){
-    int number[32];
-    decimalToBinary(num, number);
-    int index = 32-i;
-    if(number[index] == 1){
-        number[index] = 0;
-        int x = binaryToDecimal(number);
-        cout<<1<<" "<<num<<" "<<x;
-    }else{
-        number[index] = 1;
-        int x = binaryToDecimal(number);
-        cout<<0<<" "<<x<<" "<<num;
+// void bitManipulation(int num, int i){
+//     int number[32];
+//     decimalToBinary(num, number);
+//     int index = 32-i;
+//     if(number[index] == 1){
+//         number[index] = 0;
+//         int x = binaryToDecimal(number);
+//         cout<<1<<" "<<num<<" "<<x;
+//     }else{
+//         number[index] = 1;
+//         int x = binaryToDecimal(number);
+//         cout<<0<<" "<<x<<" "<<num;
+//     }
+// }
+
+void bitManipulation(int num, int i)
+{
+    int mask = 1 << (i - 1);
+
+    if (num & mask)
+    {
+        int modified = num & (~mask);
+        cout << 1 << " " << num << " " << modified;
+    }
+    else
+    {
+        int modified = num | mask;
+        cout << 0 << " " << modified << " " << num;
     }
 }
 
 int main()
 {
     int num, i;
-    cin>>num>>i;
+    cin >> num >> i;
     bitManipulation(num, i);
-    cout<<endl;
+    cout << endl;
 
-return 0;
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,29 @@
-arr
+void decimalToBinary(int n, int number[]){
+//     for(int i = 31;i>=0;i--){
+//          number[i] = n%2;
+//          n /= 2;
+//     }
+// }
+
+// int binaryToDecimal(int number[]){
+//     int decimal = 0;
+//     for(int i=0;i<32;i++){
+//         decimal = decimal * 2 + number[i];
+//     }
+//     return decimal;
+// }
+
+// void bitManipulation(int num, int i){
+//     int number[32];
+//     decimalToBinary(num, number);
+//     int index = 32-i;
+//     if(number[index] == 1){
+//         number[index] = 0;
+//         int x = binaryToDecimal(number);
+//         cout<<1<<" "<<num<<" "<<x;
+//     }else{
+//         number[index] = 1;
+//         int x = binaryToDecimal(number);
+//         cout<<0<<" "<<x<<" "<<num;
+//     }
+// }
