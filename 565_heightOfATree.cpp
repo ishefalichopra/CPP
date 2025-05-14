beca1bcb33f95aefb9fac9940905e5433eb47b90
ishefalichopra <isshefalichopra900@gmail.com>
2025-05-14 08:53:26 +0530

    height of the tree

---
+++ 565_heightOfATree.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    Node *left;
+    Node *right;
+    int data;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int height(Node* root){
+    if(!root) return -1;
+    return max(height(root->left), height(root->right)) + 1;
+}
+int main()
+{
+    
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "Height of the tree: " << height(root) << endl;
+
+return 0;
+}
