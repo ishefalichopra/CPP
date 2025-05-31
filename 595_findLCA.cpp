13881d11ab8e8cb8849f873b0598ea62ff45af90
ishefalichopra <isshefalichopra900@gmail.com>
2025-05-31 12:39:21 +0530

    find LCA Optimal

--- 595_findLCA.cpp
+++ 595_findLCA.cpp
@@ -9,30 +9,55 @@ struct Node
     Node(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
-bool findPath(Node* root, vector<Node*>& path, int k){
-    if(!root) return false;
+bool findPath(Node *root, vector<Node *> &path, int k)
+{
+    if (!root)
+        return false;
 
     path.push_back(root);
 
-    if(root->data == k) return true;
+    if (root->data == k)
+        return true;
 
-    if(root->left && findPath(root->left, path, k) || root->right && findPath(root->right, path, k)) return true;
+    if (root->left && findPath(root->left, path, k) || root->right && findPath(root->right, path, k))
+        return true;
 
     path.pop_back();
     return false;
 }
 
-Node* findLCA(Node* root, int n1, int n2){
-    
-    vector<Node*> path1, path2;
+Node *findLCA(Node *root, int n1, int n2)
+{
 
-    if(!findPath(root, path1, n1) || !findPath(root, path2, n2)) return nullptr;
+    vector<Node *> path1, path2;
+
+    if (!findPath(root, path1, n1) || !findPath(root, path2, n2))
+        return nullptr;
 
     int i;
-    for(i = 0; i < path1.size() && i < path2.size(); i++){
-        if(path1[i] != path2[i]) break;
+    for (i = 0; i < path1.size() && i < path2.size(); i++)
+    {
+        if (path1[i] != path2[i])
+            break;
     }
-    return path1[i-1];
+    return path1[i - 1];
+}
+
+Node *findLCAOptimal(Node *root, int n1, int n2)
+{
+    if (!root)
+        return nullptr;
+
+    if (root->data == n1 || root->data == n2)
+        return root;
+
+    Node *leftLCA = findLCAOptimal(root->left, n1, n2);
+    Node *rightLCA = findLCAOptimal(root->right, n1, n2);
+
+    if (leftLCA && rightLCA)
+        return root;
+
+    return (leftLCA != nullptr) ? leftLCA : rightLCA;
 }
 
 int main()
@@ -46,7 +71,8 @@ int main()
     root->right->right = new Node(7);
 
     int n1 = 4, n2 = 5;
-    Node *lca = findLCA(root, n1, n2);
+    // Node *lca = findLCA(root, n1, n2);
+    Node* lca = findLCAOptimal(root, n1, n2);
     if (lca)
         cout << "LCA of " << n1 << " and " << n2 << " is: " << lca->data << endl;
     else
