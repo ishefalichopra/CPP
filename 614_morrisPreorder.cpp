750b4def2551357db17a97c809a59f9d7abd44d1
ishefalichopra <isshefalichopra900@gmail.com>
2025-06-05 09:24:46 +0530

    morris preorder traversal

---
+++ 614_morrisPreorder.cpp
@@ -0,0 +1,45 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left, *right;
+    Node(int x) : data(x), left(nullptr), right(nullptr) {}
+};
+
+void morris_preorder(Node* root){
+    Node* curr = root;
+    while(curr){
+        if(!curr->left){
+            cout<<curr->data<<" ";
+            curr = curr->right;
+        }else{
+            Node* pred = curr->left;
+            while(pred->right && pred->right != curr){
+                pred = pred->right;
+            }
+            if(!pred->right){
+                cout<<curr->data<<" ";
+                pred->right = curr;
+                curr = curr->left;
+            }else{
+                pred->right = nullptr;
+                curr = curr->right;
+            }
+        }
+    }
+}
+
+int main()
+{  
+    Node *root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(15);
+    root->left->left = new Node(3);
+    root->left->right = new Node(7);
+    root->right->right = new Node(20);
+    morris_preorder(root);
+   
+return 0;
+}
