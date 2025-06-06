06b04e063aab38c01c114d349047231ed2ce56b7
ishefalichopra <isshefalichopra900@gmail.com>
2025-06-06 13:01:17 +0530

    median of a BST in O(1) space

--- 619_medianOfBST.cpp
+++ 619_medianOfBST.cpp
@@ -15,21 +15,92 @@ void inorder(Node *root, vector<int> &in)
     in.push_back(root->data);
     inorder(root->right, in);
 }
+float inorder_morris(Node* root){
+    int n = 0;
+    Node* curr = root;
+    while(curr){
+        if(!curr->left){
+            n++;
+            curr = curr->right;
+        }else{
+            Node* pred = curr->left;
+            
+            while(pred->right && pred->right != curr){
+                pred = pred->right;
+            }
+            
+            if(!pred->right){
+                pred->right = curr;
+                curr = curr->left;
+            }else{
+                pred->right = nullptr;
+                n++;
+                curr = curr->right;
+            }
+        }
+    }
+    
+    int cnt = 0;
+    float median = 0;
+    Node* current = root;
+    while(current){
+        if(!current->left){
+            cnt++;
+            
+            if(n%2 != 0 && cnt == (n+1)/2){
+                return current->data;
+            }
+            
+            if(n%2 == 0 && (cnt == n/2 || cnt == (n/2+1))){
+                if(median == 0) median = current->data;
+                else return (median + current->data)/2.0;
+            }
+            current = current->right;
+        }else{
+            Node* pre = current->left;
+            
+            while(pre->right && pre->right != current){
+                pre = pre->right;
+            }
+            
+            if(!pre->right){
+                pre->right = current;
+                current = current->left;
+            }else{
+                pre->right = nullptr;
+                cnt++;
+            
+            if(n%2 != 0 && cnt == (n+1)/2){
+                return current->data;
+            }
+            
+            if(n%2 == 0 && (cnt == n/2 || cnt == (n/2+1))){
+                if(median == 0) median = current->data;
+                else return (median + current->data)/2.0;
+            }
+                current = current->right;
+            }
+        }
+    }
+    return -1;
+}
 float findMedian(struct Node *root)
 {
-    vector<int> in;
-    inorder(root, in);
+    // vector<int> in;
+    // inorder(root, in);
 
-    int n = in.size();
+    // int n = in.size();
 
-    if (n % 2 == 0)
-    {
-        return (in[n / 2] + in[n / 2 - 1]) / 2.0;
-    }
-    else
-    {
-        return in[n / 2];
-    }
+    // if (n % 2 == 0)
+    // {
+    //     return (in[n / 2] + in[n / 2 - 1]) / 2.0;
+    // }
+    // else
+    // {
+    //     return in[n / 2];
+    // }
+
+    return inorder_morris(root);
 }
 
 int main()
