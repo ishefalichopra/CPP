9bf852ae9e89f93e28ca7ed5bdb4fb5d7ccb3c55
ishefalichopra <isshefalichopra900@gmail.com>
2025-06-07 07:41:34 +0530

    get coutn using morris traversal

--- 620_countBSTinRange.cpp
+++ 620_countBSTinRange.cpp
@@ -7,18 +7,60 @@ struct Node
     Node(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
-
-int getCount(Node* root, int l, int h){
-    if(!root) return 0;
+int getCount(Node *root, int l, int h)
+{
+    if (!root)
+        return 0;
     int cnt = 0;
-    if(root->data >= l && root->data <= h) {
+    if (root->data >= l && root->data <= h)
+    {
         cnt = 1;
     }
-    if(root->data > l) {
+    if (root->data > l)
+    {
         cnt += getCount(root->left, l, h);
     }
-    if(root->data < h) {
+    if (root->data < h)
+    {
         cnt += getCount(root->right, l, h);
+    }
+    return cnt;
+}
+
+int getCountOptimized(Node *root, int l, int h)
+{
+    int cnt = 0;
+    Node *curr = root;
+    while (curr)
+    {
+        if (!curr->left)
+        {
+            if (curr->data >= l && curr->data <= h)
+                cnt++;
+            curr = curr->right;
+        }
+        else
+        {
+            Node *pred = curr->left;
+
+            while (pred->right && pred->right != curr)
+            {
+                pred = pred->right;
+            }
+
+            if (!pred->right)
+            {
+                pred->right = curr;
+                curr = curr->left;
+            }
+            else
+            {
+                pred->right = nullptr;
+                if (curr->data >= l && curr->data <= h)
+                    cnt++;
+                curr = curr->right;
+            }
+        }
     }
     return cnt;
 }
@@ -33,8 +75,8 @@ int main()
     root->right->left = new Node(5);
     root->right->right = new Node(7);
     int l = 2, h = 5;
-    cout << "Count of nodes in range [" << l << ", " << h << "] is: " 
-         << getCount(root, l, h) << endl;
+    cout << "Count of nodes in range [" << l << ", " << h << "] is: "
+         << getCountOptimized(root, l, h) << endl;
 
     return 0;
 }
